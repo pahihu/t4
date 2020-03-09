edcd05bd9a8b25bfc1484b82f835ac1fc812299a
Andras Pahi <pahia@t-online.hu>
2020-03-09 11:51:26 +0100

    added D7214 C Toolset itools

---
+++ itools/d7214/d7214-env.sh
@@ -0,0 +1,10 @@
+#
+# INMOS D7214C C Toolset
+#
+export D7205=$HOME/d7205
+export D7214=$HOME/ictools
+
+export PATH=$D7214/itools:$D7205/itools:$PATH
+export ISEARCH="$D7214/libs/;$D7205/libs/"
+export IBOARDSIZE=#200000
+export ITERM=$D7205/iterms/ansi.itm

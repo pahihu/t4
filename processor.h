fde0d4008386fcfddc66bb54e2ff2905421d8083
Andras Pahi <pahia@t-online.hu>
2023-05-16 20:42:40 +0200

    changed SharedChannel length and data placement

--- processor.h
+++ processor.h
@@ -94,8 +94,8 @@ extern uint32_t CoreSize;
 #define MAX_CHANNEL_URL 128
 
 #define MAX_DATA        255
-#define SCH_LEN         0
-#define SCH_DATA        1
+#define SCH_LEN         255
+#define SCH_DATA        0
 #define SCH_SIZE        (MAX_DATA+1)
 
 typedef struct _Channel_ {

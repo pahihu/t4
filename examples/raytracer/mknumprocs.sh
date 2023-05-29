28c4ed78f5070958ecff7e2d6b7c9827c622012d
Andras Pahi <pahia@t-online.hu>
2023-05-29 21:16:27 +0200

    fixed raytracer/ generated files

--- examples/raytracer/mknumprocs.sh
+++ examples/raytracer/mknumprocs.sh
@@ -2,7 +2,9 @@ for x in 3 4 6 10 11 18 34
 do
   echo "using $x processors..."
   rm -f raytraceT8.pgm
-  sed -e "s/NUMPROCS/$x/g" raytraceT8.pgm.temp > raytraceT8.pgm
+  y=$(($x-2))
+  echo "NUMPROCS=$y"
+  sed -e "s/NUMPROCS/$y/g" raytraceT8.pgm.temp > raytraceT8.pgm
   make clean
   make
   cp raytraceT8.btl raytrace$x.btl
